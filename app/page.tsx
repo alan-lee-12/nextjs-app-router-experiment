37a66faaebf38560faa5c672fc73c1a68b02bf7f
Alan Lee <alanlee@alee-mbp-14.local>
2023-09-08 17:02:36 -0700

    add Tester page.

--- app/page.tsx
+++ app/page.tsx
@@ -115,13 +115,30 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Testing..{' '}
+            Info{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             I want to route to Info page..
+          </p>
+        </a>
+
+        <a
+          href="/tester"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Tester{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Tester page
           </p>
         </a>
       </div>
---
+++ app/tester/page.tsx
@@ -0,0 +1,6 @@
+const Tester = () =>
+    <>
+        <p> just some text </p>
+    </>
+
+export default Tester
